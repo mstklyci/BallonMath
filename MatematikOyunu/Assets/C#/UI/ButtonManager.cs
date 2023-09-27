5ce861785df3c8eaf1a4280fa5ea6a4121824b44
Mesut Kalayci <124871532+mstklyci@users.noreply.github.com>
2023-09-27 14:58:23 +0300

    .

---
+++ MatematikOyunu/Assets/C#/UI/ButtonManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ButtonManager : MonoBehaviour
+{
+    public AudioSource butonsespanali;
+    public AudioClip Butonsesi;
+
+    public int sahne = 0;
+    private bool duraklatma = false;
+    public GameObject MenuPanel;
+    public void MenuButton()
+    {
+        butonsespanali.PlayOneShot(Butonsesi);
+        SceneManager.LoadScene(0);
+    }
+    public void RestartButton()
+    {
+        butonsespanali.PlayOneShot(Butonsesi);
+        SceneManager.LoadScene(sahne);
+    }
+    public void Durdur()
+    {
+        if (!duraklatma)
+        {
+            butonsespanali.PlayOneShot(Butonsesi);
+            MenuPanel.SetActive(true);
+            Time.timeScale = 0;
+            duraklatma = true;
+        }
+        else
+        {
+            butonsespanali.PlayOneShot(Butonsesi);
+            MenuPanel.SetActive(false);
+            Time.timeScale = 1;
+            duraklatma = false;
+        }
+    }
+}
